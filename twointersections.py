d5ccc5e9ff35ed6152e0e0f863227d18e75ea36d
chamath925 <sunimaldk@gmail.com>
2025-05-02 19:42:14 +0530

    twointersections

---
+++ twointersections.py
@@ -0,0 +1,8 @@
+setx = {"green","blue"}
+sety = {"blue","yellow"}
+print("original set element")
+print(setx)
+print(sety)
+print("/n two intersections of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)
